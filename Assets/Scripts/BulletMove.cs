3b9363c0fa9d596041cb0379f4b5b9a54e0c2429
Yang Liu <Yang Liu>
2016-07-20 12:30:20 -0400

    Player can now shoot discs.

---
+++ Assets/Scripts/BulletMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMove : MonoBehaviour {
+    public float maxSpeed = 5f;
+    public Vector3 originalSpeed = new Vector3(0f,0f,0f);
+
+    GameObject playerObject;
+	
+    void Start() {
+        playerObject = GameObject.FindWithTag("Player");
+    }
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, maxSpeed * Time.deltaTime, 0);
+
+        pos += transform.rotation * velocity + originalSpeed * Time.deltaTime;
+
+        transform.position = pos;
+
+        // if the bullet is far away from the player,
+        // destroy it
+        float distanceFromPlayer = (playerObject.transform.position - transform.position).magnitude;
+        if (distanceFromPlayer > 10) {
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+    public GameObject bulletPrefab;
+    public float fireDelay = 0.25f;
+
+    float cooldownTimer = 0;
+    Vector3 lastFramePos = new Vector3(0f, 0f, 0f);
+	
+	// Update is called once per frame
+	void Update () {
+        cooldownTimer -= Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1") && cooldownTimer <= 0) {
+            //Debug.Log("Pew!");
+            cooldownTimer = fireDelay;
+
+            // Get the difference between the mouse and the player
+            Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            difference.Normalize();
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            Quaternion bulletRotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+
+            Object newBullet = Instantiate(bulletPrefab, transform.position, bulletRotation);
+            
+        }
+	}
+}
